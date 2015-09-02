2baa1cc9b396b0907ceee514538c7aaf6e3483bf
Michael Teagle <i68066@mteagle-G3QP.local>
2015-09-02 10:26:46 -0600

    changed a lot of styling for second menu, added javascript to second menu, eveything is happy!

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by i68066 on 9/2/15.
+ */
+
+window.onload = function () {
+    myID = document.getElementById("secondMenu");
+};
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 44) {
+       myID.className = "show"
+    } else {
+        myID.className = "top"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
