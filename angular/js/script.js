906eb4eb5a40d82c33b7e46308b4834d9b324b67
Michael Teagle <i68066@mteagle-G3QP.local>
2015-09-16 10:05:28 -0600

    cloned angular material from github.

---
+++ angular/js/script.js
@@ -0,0 +1,31 @@
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']).controller('AccordionDemoCtrl', function ($scope) {
+    var items={};
+
+    $scope.oneAtATime = true;
+
+    $scope.groups = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
+    $scope.deleteItem = function (index) {
+        $scope.items.splice(index, 1);
+    }
+
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+});
